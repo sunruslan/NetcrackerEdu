4d4f046a00fd4d329a62191c72ddfb9a11e67cdb
User <User@nb0414.tcsbank.ru>
2018-07-20 14:41:21 +0300

    update

--- JavaBasics/Archiver/src/Archiver.java
+++ JavaBasics/Archiver/src/Archiver.java
@@ -11,57 +11,56 @@ public class Archiver {
     private static void instruction () {
         System.out.println("Instruction");
     }
-    private static void compress (String[] args) throws NotValidInput {
-        if (!validToCompress(args)) {
-            throw new NotValidInput();
-        }
+    private static void compress (String[] args){
         try {
-            ZipOutputStream zipout = new ZipOutputStream(new FileOutputStream(args[1]));
+            ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(args[1]));
             for (int i = 2; i < args.length; ++i) {
                 FileInputStream fin = new FileInputStream(args[i]);
                 ZipEntry entry = new ZipEntry(args[i]);
-                zipout.putNextEntry(entry);
+                zout.putNextEntry(entry);
                 byte[] buffer = new byte[fin.available()];
                 fin.read(buffer);
-                zipout.write(buffer);
-                zipout.closeEntry();
+                zout.write(buffer);
+                zout.closeEntry();
                 fin.close();
             }
-            zipout.close();
+            zout.close();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
-    private static void extract (String[] args) throws NotValidInput {
-        if (!validToExtract(args)) {
-            throw new NotValidInput();
-        }
+    private static void extract (String[] args){
         try {
-            ZipInputStream zipin = new ZipInputStream(new FileInputStream(args[1]));
+            ZipInputStream zin = new ZipInputStream(new FileInputStream(args[1]));
             ZipEntry entry;
-            while ((entry = zipin.getNextEntry()) != null) {
+            while ((entry = zin.getNextEntry()) != null) {
                 String name = entry.getName();
                 FileOutputStream fout = new FileOutputStream(args.length==3?args[2].concat(name):name);
-                for (int c = zipin.read(); c != -1; c = zipin.read()) {
+                for (int c = zin.read(); c != -1; c = zin.read()) {
                     fout.write(c);
                 }
                 fout.flush();
-                zipin.closeEntry();
+                zin.closeEntry();
                 fout.close();
             }
         } catch(Exception ex) {
             ex.printStackTrace();
         }
     }
-    private static void add (String[] args) throws NotValidInput {
-        if (!validToAdd(args)) {
-            throw new NotValidInput();
+    private static void add (String[] args){
+        try {
+            ZipInputStream zin = new ZipInputStream(new FileInputStream(args[1]));
+            ZipOutputStream zout = new ZipOutputStream(new FileOutputStream("temporary.zip"));
+            ZipEntry entry;
+            while ((entry = zin.getNextEntry()) != null) {
+
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
-    private static void comment (String[] args) throws NotValidInput {
-        if (!validToComment(args)) {
-            throw new NotValidInput();
-        }
+    private static void comment (String[] args){
         if (args[2].equals("-r")) {
             try {
                 ZipFile zip = new ZipFile(new File(args[2]), ZipFile.OPEN_READ);
@@ -71,50 +70,22 @@ public class Archiver {
             }
         } else if (args[2].equals("-a")) {
             try {
-                ZipOutputStream zipout = new ZipOutputStream(new FileOutputStream(args[2]));
-                zipout.setComment(args[3]);
+                ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(args[2]));
+                zout.setComment(args[3]);
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
         }
     }
-    private static boolean validToCompress (String[] args) {
-        return true;
-    }
-    private static boolean validToExtract (String[] args) {
-        return true;
-    }
-    private static boolean validToComment (String[] args) {
-        return true;
-    }
-    private static boolean validToAdd (String[] args) {
-        return true;
-    }
     public static void main(String[] args) {
         if (args != null && args[0].equals("--compress")) {
-            try {
-                compress(args);
-            } catch (NotValidInput e){
-                System.err.println();
-            }
+            compress(args);
         } else if (args != null && args[0].equals("--extract")) {
-            try {
-                extract(args);
-            } catch (NotValidInput e){
-                System.err.println();
-            }
+            extract(args);
         } else if (args != null && args[0].equals("--comment")) {
-            try {
-                comment(args);
-            } catch (NotValidInput e){
-                System.err.println();
-            }
+            comment(args);
         } else if (args != null && args[0].equals("--add")) {
-            try {
-                add(args);
-            } catch (NotValidInput e){
-                System.err.println();
-            }
+            add(args);
         } else {
             instruction();
         }
